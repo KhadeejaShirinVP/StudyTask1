160ec3ec9c3fd72022a6342c29f314c435b507c4
Khadeeja-Shirin <shirinalnajm@gmail.com>
2022-09-27 21:48:52 +0530

    myTask

---
+++ fundamentalTask/CharContains/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharContains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, k;
+            Console.WriteLine("Enter a word");
+            s= Console.ReadLine();
+            Console.WriteLine("Please enter the character you want to check?");
+            k= Console.ReadLine();
+            if (s.Contains(k))
+                Console.WriteLine(k + " found!");
+            else
+                Console.WriteLine(k + " not found!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ fundamentalTask/PrimeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsPrime = true;
+            Console.WriteLine("Prime Numbers Between 1 To 100: ");
+            for (int k = 2; k <= 100; k++)
+            {
+                for (int l = 2; l <= k; l++)
+                {
+                    if (k != l && k % l == 0)
+                    {
+                        IsPrime = false;
+                        break;
+                    }
+                }
+                if (IsPrime)
+                Console.Write("\n" + k);
+                IsPrime = true;
+            } Console.ReadKey();
+        }
+    }
+}
